7bb228fcd402d021652da7389ecfe3f4a30d184b
daniel <daniel@Daniels-MacBook-Pro-2.local>
2015-02-05 14:29:48 -0600

    fixing the nav color and transparency

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,57 @@
+// VENDOR
+
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+
+//  LIB
+//= require behavior
+
+
+// APP
 //= require_tree .
+
+function setupNavColorForTeam() {
+  if ($.behavior("color-for-nav") != undefined) {
+    var color = $.behavior("color-for-nav").val(),
+        $nav = $('.navbar-default');
+    $nav.css({"background-color":  color});
+  }
+}
+
+function setupTransparentHeaderWhenSplash() {
+  var $splash = $.behavior("splash"),
+      $nav = $('.navbar-default');
+
+  if ($splash.length == 0 || $splash == undefined) {
+    $nav.removeClass('transparent');
+  } else {
+    $nav.addClass("transparent");
+
+    var splashbottom = $splash.offset().top + $splash.height() - 100;
+
+    $(window).on('scroll',function(){
+      stop = Math.round($(window).scrollTop());
+
+      if (stop > splashbottom) {
+        if ($nav.hasClass("transparent")) {
+          $nav.removeClass("transparent");
+        }
+      } else {
+        if (!$nav.hasClass("transparent")) {
+          $nav.addClass("transparent");
+        }
+      }
+    });
+  }
+}
+
+$(document).on("ready page:load fragment:load", function() {
+  setupNavColorForTeam();
+  setupTransparentHeaderWhenSplash();
+});
+
+
+
+
+  // on scroll,
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def body_css_class
+    controller = controller_path.gsub("/","-").underscore.dasherize
+    "#{controller}-body"
+  end
+
+
 end
