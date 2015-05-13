b02c672c903c365aacb210b791950705400bd88f
Paul Gonzalez <pjg5005@gmail.com>
2015-05-13 11:46:31 -0500

    Updated events index to add dates

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,4 +9,17 @@ class Event < ActiveRecord::Base
   def live_tailgates
     self.tailgates.where("current_size > ?", 0) && self.tailgates.where("live = ?", true)
   end
+
+  def home_team
+    if self.home_team_id.present?
+      Team.find(self.home_team_id)
+    end
+  end
+
+  def away_team
+    if self.away_team_id.present?
+      Team.find(self.away_team_id)
+    end
+  end
+
 end
